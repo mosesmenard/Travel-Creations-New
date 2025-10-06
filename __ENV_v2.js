154a6c176d71110d66f120632af2247986d656d2
mosesmenard <menardowuor@gmail.com>
2025-10-06 12:49:08 +0300

    Undo flight search update

--- __ENV_v2.js
+++
@@ -1,8 +0,0 @@
-window.__ENV = {
-  BASE_URL: "https://travelcreations.golibe.com",
-  LANGUAGE: "en",
-  COUNTRY: "KE",
-  CURRENCY: "KES",
-  IBE_URL: "https://travelcreations.golibe.com/engine/",
-  MODE: "production"
-};
