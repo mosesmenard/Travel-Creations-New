069a1791944b0a75a4cfc3bfec457ae9329e1d7b
mosesmenard <menardowuor@gmail.com>
2025-10-06 11:57:04 +0300

    updated env

---
+++ __ENV_v2.js
@@ -0,0 +1,8 @@
+window.__ENV = {
+  BASE_URL: "https://travelcreations.golibe.com",
+  LANGUAGE: "en",
+  COUNTRY: "KE",
+  CURRENCY: "KES",
+  IBE_URL: "https://travelcreations.golibe.com/engine/",
+  MODE: "production"
+};
